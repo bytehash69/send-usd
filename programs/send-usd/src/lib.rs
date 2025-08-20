41f0b04726340381bdb9e01f1654ddae1038b7e9
Byte <bytehash69@gmail.com>
2025-08-20 19:37:59 +0530

    Tx-send

--- programs/send-usd/src/lib.rs
+++ programs/send-usd/src/lib.rs
@@ -1,16 +1,62 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{prelude::*, solana_program::native_token::LAMPORTS_PER_SOL};
+use pyth_solana_receiver_sdk::price_update::{get_feed_id_from_hex, PriceUpdateV2};
 
 declare_id!("AbNFkgmcKiKTeyFoGAMagHxWo3ZMTuc2KnoQkUmJqh1V");
 
+pub const MAXIMUM_AGE: u64 = 3600; // 1 hour
+pub const FEED_ID: &str = "0xef0d8b6fda2ceba41da15d4095d1da392a0d2f8ed0c6c7bc0f4cfac8c280b56d";
+
 #[program]
 pub mod send_usd {
+    use anchor_lang::solana_program::{
+        program::{invoke, invoke_signed, invoke_signed_unchecked},
+        system_instruction::transfer,
+    };
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn send(ctx: Context<Send>, amount_in_usd: u64) -> Result<()> {
+        let price_update = &mut ctx.accounts.price_upgrade;
+        let price = price_update.get_price_no_older_than(
+            &Clock::get()?,
+            MAXIMUM_AGE,
+            &get_feed_id_from_hex(FEED_ID)?,
+        )?;
+
+        let amount_in_lamports = LAMPORTS_PER_SOL
+            .checked_mul(10_u64.pow(price.exponent.abs().try_into().unwrap()))
+            .unwrap()
+            .checked_mul(amount_in_usd)
+            .unwrap()
+            .checked_div(price.price.try_into().unwrap())
+            .unwrap();
+
+        let ix = transfer(
+            ctx.accounts.payer.key,
+            ctx.accounts.destination.key,
+            amount_in_lamports,
+        );
+
+        invoke(
+            &ix,
+            &[
+                ctx.accounts.payer.to_account_info(),
+                ctx.accounts.destination.to_account_info(),
+            ],
+        )?;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(amount_in_usd: u64)]
+pub struct Send<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    #[account(mut)]
+    pub destination: SystemAccount<'info>,
+    pub price_upgrade: Account<'info, PriceUpdateV2>,
+    pub system_program: Program<'info, System>,
+}
